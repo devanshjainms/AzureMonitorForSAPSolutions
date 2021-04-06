b58ef05a167a6055c556dd577ff2b2ce0c84b8af
Syeda Persia Aziz <syaziz@microsoft.com>
2021-04-05 22:59:39 -0700

    vcguidbody

--- sapmon/payload/provider/hcx.py
+++ sapmon/payload/provider/hcx.py
@@ -34,6 +34,7 @@ class hcxProviderInstance():
         headers['x-hm-authorization'] = self.authToken
         response = requests.get(vcGUIDURL, headers=headers, verify=False)
         vcguidbody = response.json()
+        self.tracer.info("vcguid body",vcguidbody)
         self.vcGUID = str(vcguidbody[0]) if len(vcguidbody) > 0 else None
         if self.vcGUID == None:
             self.tracer.info("vcguid not found!!!!!!")
